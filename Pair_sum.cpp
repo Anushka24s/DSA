d62b77cebdcba6be2432b77cfbaf563a863f603d
Anushka <anushka132407@gmail.com>
2025-06-25 00:18:38 +0530

    Merge branch 'main' of https://github.com/Anushka24s/DSA

---
+++ Pair_sum.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> pairSum(vector<int> &arr, int s)
+{
+    // Write your code here.
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[i] + arr[j] == s)
+            {
+                vector<int> temp;
+                temp.push_back(min(arr[i], arr[j]));
+                temp.push_back(max(arr[i], arr[j]));
+                ans.push_back(temp);
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+// or if we want to only return a single pair without any sorting constraint
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                    return {i, j};
+            }
+        }
+
+        return {};
+    }
+};
+
+int main()
+{
+}
